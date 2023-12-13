8e2b95c3c20fb72b39b1ef163cbc9494a1ce7455
Jaime Rodriguez <rodriguezpuertajaime@gmail.com>
2023-12-13 19:57:34 +0100

    Add Log in button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { FaChevronRight } from "react-icons/fa6";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import Button from "./components/Button/Button";
+import {LogInButton} from "./components/logInButton/logInButton"
+import { TbLego } from "react-icons/tb";
 
 function App() {
   return (
@@ -17,7 +19,14 @@ function App() {
         iconLeft={<FaArrowLeftLong />}
         handleClick={() => console.log("router.push(/login)")}
       />
+      <LogInButton
+        label="Iniciar sesión"
+        href=""
+        icon={<TbLego/>}
+
+      />
     </>
+
   );
 }
 
---
+++ src/components/logInButton/logInButton.jsx
@@ -0,0 +1,28 @@
+import styles from "./logInButton.module.scss";
+
+
+export const LogInButton = ({ label, icon, href}) => {
+    const a = <div className="cara">
+        <svg stroke="currentColor"
+        fill="none" 
+        stroke-width="2" 
+        viewBox="0 0 24 24" 
+        stroke-linecap="round" 
+        stroke-linejoin="round" 
+        height="1em" width="1em" 
+        xmlns="http://www.w3.org/2000/svg">
+        <path d="M9.5 11l.01 0"></path>
+        <path d="M14.5 11l.01 0"></path>
+        <path d="M9.5 15a3.5 3.5 0 0 0 5 0"></path>
+        </svg>
+    </div>
+    return (
+        <>
+            <a className={styles.LogInButton} href={href} target="_blank">
+                <>{icon}</>
+                <>{a}</>
+                <p>{label}</p>
+            </a>
+        </>
+    )
+}
