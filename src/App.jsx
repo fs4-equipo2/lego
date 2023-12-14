4218901151853f0d70455063b29c2d711ec0be71
mariacortes <mariacv60@hotmail.com>
2023-12-14 10:56:04 +0100

    feat/iconos_listos

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import { FaChevronRight } from "react-icons/fa6";
-import { FaArrowLeftLong } from "react-icons/fa6";
-import Button from "./components/Button/Button";
 import { Iconos } from "./components/Iconos/Iconos";
 import { FaXTwitter } from "react-icons/fa6";
+import { CgFacebook } from "react-icons/cg";
+import { IoLogoInstagram } from "react-icons/io5";
+import { FaYoutube } from "react-icons/fa";
 
 function App() {
   return (
-    <>
-      <Button
-        texto={"Ofertas"}
-        isMain={true}
-        iconRight={<FaChevronRight />}
-        handleClick={() => console.log("router.push(/login)")}
-      />
-      <Button
-        texto={"ZONA DE JUEGOS"}
-        isGamezone={true}
-        iconLeft={<FaArrowLeftLong />}
-        handleClick={() => console.log("router.push(/login)")}
+    <div>
+       <h2>Iconos Reds Sociales</h2>
+      <Iconos
+        icono={<CgFacebook />}
+        href={"https://www.facebook.com/LEGOEsp/"}
       />
       <Iconos icono={<FaXTwitter />} href={"https://twitter.com/LEGO_Group"} />
-    </>
+      <Iconos
+        icono={<IoLogoInstagram />}
+        href={"https://www.instagram.com/lego"}
+      />
+      <Iconos icono={<FaYoutube />} href={"https://m.youtube.com/user/LEGO"} />
+    </div>
   );
 }
 
