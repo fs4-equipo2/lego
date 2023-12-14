a34ed86669812c406e18eec26f8bb12e6fb2ecd0
manuelbenitez <manu.benitez1@hotmail.com>
2023-12-14 11:10:10 +0100

    refactor: LogInButton

--- src/components/logInButton/logInButton.jsx
+++ src/components/logInButton/logInButton.jsx
@@ -1,28 +1,14 @@
+import IconoCara from "./IconoCara";
 import styles from "./logInButton.module.scss";
 
-
-export const LogInButton = ({ label, icon, href}) => {
-    const a = <div className="cara">
-        <svg stroke="currentColor"
-        fill="none" 
-        stroke-width="2" 
-        viewBox="0 0 24 24" 
-        stroke-linecap="round" 
-        stroke-linejoin="round" 
-        height="1em" width="1em" 
-        xmlns="http://www.w3.org/2000/svg">
-        <path d="M9.5 11l.01 0"></path>
-        <path d="M14.5 11l.01 0"></path>
-        <path d="M9.5 15a3.5 3.5 0 0 0 5 0"></path>
-        </svg>
-    </div>
-    return (
-        <>
-            <a className={styles.LogInButton} href={href} target="_blank">
-                <>{icon}</>
-                <>{a}</>
-                <p>{label}</p>
-            </a>
-        </>
-    )
-}
+export const LogInButton = ({ label, icon, href }) => {
+  return (
+    <>
+      <a className={styles.LogInButton} href={href} target="_blank">
+        <>{icon}</>
+        <IconoCara />
+        <p>{label}</p>
+      </a>
+    </>
+  );
+};
