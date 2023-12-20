2a9ecda992c058c10f88c198b3f2b15866cb8feb
manuelbenitez <manu.benitez1@hotmail.com>
2023-12-20 11:27:48 +0100

    chore: refactorizar codigo

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,11 @@
-import './Card.module.scss'
+import "./Card.module.scss";
 
-export function Card({src, title, content, button}){
-    return <div className="cardContainer">
-       <img src = {src} />
-       <h3>{title}</h3>
-       <p>
-          {content}
-        </p>
+export function Card({ src, title, content, button }) {
+  return (
+    <div className="cardContainer">
+      <img src={src} />
+      <h3>{title}</h3>
+      <p>{content}</p>
     </div>
+  );
 }
