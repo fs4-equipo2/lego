9017a4527cc28979aaf0ffa3cb038ef755190ed9
manuelbenitez <manu.benitez1@hotmail.com>
2024-01-25 11:50:58 +0100

    refactor: app y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,22 @@
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import { CardRecomendados } from "./components/CardRecomendados/CardRecomendados";
 import { Blog } from "./components/Blog/Blog";
 import { BlogSelecciones } from "./components/BlogSelecciones/BlogSelecciones";
 import { BlogRecomendados } from "./components/BlogRecomendados/BlogRecomendados";
-import Footer from './components/Footer/Footer';
 import { Stickers } from "./components/Stickers/Stickers";
 import { WelcomeMessage } from "./components/WelcomeMessage/WelcomeMessage";
 import Main from "./components/Main/Main";
 function App() {
-  const productos = [
-    {
-      src: "https://www.lego.com/cdn/cs/set/assets/bltb7d457aa2ce9f859/40499_Prod.png?fit=bounds&format=webply&quality=80&width=320&height=320&dpr=1",
-      title: "Trineo de Papá Noel",
-      price: "39,99€",
-      button: "Añadir a la bolsa",
-      link: "link",
-      uid: "3b241101-e2bb-4255-8caf-4136c566a962",
-    },
-    {
-      src: "https://www.lego.com/cdn/cs/set/assets/bltb7d457aa2ce9f859/40499_Prod.png?fit=bounds&format=webply&quality=80&width=320&height=320&dpr=1",
-      title: "Trineo de Papá Noel",
-      price: "39,99€",
-      button: "Añadir a la bolsa",
-      link: "link",
-      uid: "3b241101-e2bb-4255-8caf-4136c566a962",
-    },
-    {
-      src: "https://www.lego.com/cdn/cs/set/assets/bltb7d457aa2ce9f859/40499_Prod.png?fit=bounds&format=webply&quality=80&width=320&height=320&dpr=1",
-      title: "Trineo de Papá Noel",
-      price: "39,99€",
-      button: "Añadir a la bolsa",
-      link: "link",
-      uid: "3b241101-e2bb-4255-8caf-4136c566a962",
-    },
-    {
-      src: "https://www.lego.com/cdn/cs/set/assets/bltb7d457aa2ce9f859/40499_Prod.png?fit=bounds&format=webply&quality=80&width=320&height=320&dpr=1",
-      title: "Trineo de Papá Noel",
-      price: "39,99€",
-      button: "Añadir a la bolsa",
-      link: "link",
-      uid: "3b241101-e2bb-4255-8caf-4136c566a962",
-    },
-    {
-      src: "https://www.lego.com/cdn/cs/set/assets/bltb7d457aa2ce9f859/40499_Prod.png?fit=bounds&format=webply&quality=80&width=320&height=320&dpr=1",
-      title: "Trineo de Papá Noel",
-      price: "39,99€",
-      button: "Añadir a la bolsa",
-      link: "link",
-      uid: "3b241101-e2bb-4255-8caf-4136c566a962",
-    },
-  ];
   return (
     <>
       <Navbar />
-      <CardRecomendados
-        src="https://www.lego.com/cdn/cs/set/assets/bltb7d457aa2ce9f859/40499_Prod.png?fit=bounds&format=webply&quality=80&width=320&height=320&dpr=1"
-        title="Trineo de Papá Noel"
-        price="39,99€"
-        button="Añadir a la bolsa"
-        link="link"
-        uid="3b241101-e2bb-4255-8caf-4136c566a962"
-      />
-      {productos.map((producto) => (
-        <CardRecomendados
-          src={producto.src}
-          title={producto.title}
-          price={producto.price}
-          button={producto.button}
-          link={producto.link}
-          uid={producto.uid}
-        />
-      ))}
       <Main />
       <Stickers />
       <BlogSelecciones />
-      <BlogRecomendados/>
-      <Blog/>
-      <BlogSelecciones/>
-      <BlogRecomendados/>
+      <BlogRecomendados />
+      <Blog />
+      <BlogSelecciones />
+      <BlogRecomendados />
       <Blog />
       <WelcomeMessage />
       <Footer />
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,24 +6,12 @@ import Tipografia from "../Tipografia/Tipografia";
 import { Iconos } from "../Iconos/Iconos";
 import { CgFacebook } from "react-icons/cg";
 import { IoLogoInstagram } from "react-icons/io5";
-import FooterColumn from "./FooterColumn";
-import {
-  quienessomos,
-  column,
-  atencionAlCliente,
-  atracciones,
-  masImformacion,
-  links,
-} from "./FooterList";
-import { FooterLinks } from "./FooterLinks";
-import { FooterColumnResponsive } from "./FooterColumnResponsive";
-
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.top}>
         <div className={styles.Container}>
-          <div className={styles.firstColumn}>
+          <div>
             <div className={styles.LegoIcon}>
               <LegoLogo />
             </div>
@@ -34,12 +22,6 @@ const Footer = () => {
                   width="17"
                   height="24"
                   viewBox="0 0 17 24"
-
-              <button className={styles.localizacion}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 19 25"
-
                   aria-hidden="true"
                   class="Icon__StyledSVG-lm07h6-0 eqVDRK RegionSelectorstyles__PointerIcon-sc-14dxhlg-2 izbKLk"
                 >
@@ -96,7 +78,6 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-
           <div>
             <a href=" " className={styles.title}>
               <Tipografia
@@ -369,127 +350,35 @@ const Footer = () => {
                 </a>
               </li>
             </ul>
-                <Tipografia
-                  color={"--white"}
-                  texto={"España"}
-                  isBodyMedium
-                  isRegularWeight
-                />
-              </button>
-            </div>
-            <FooterColumn title={column.title} items={column.items} />
-          </div>
-          <div className={styles.groupColumn}>
-            <div className={styles.groupColumnTop}>
-              <FooterColumn
-                title={quienessomos.title}
-                items={quienessomos.items}
-              />
-              <FooterColumn
-                title={atencionAlCliente.title}
-                items={atencionAlCliente.items}
-              />
-            </div>
-            <div className={styles.groupColumnBottom}>
-              <FooterColumn
-                title={atracciones.title}
-                items={atracciones.items}
-              />
-              <FooterColumn
-                title={masImformacion.title}
-                items={masImformacion.items}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={styles.midResponsive}>
-        <div className={styles.ContainerResponsive}>
-          <div className={styles.emailResponsive}>
-            <Tipografia
-              color={"--white"}
-              texto={"SUSCRÍBETE AL CORREO ELECTRÓNICO DE LEGO® SHOP"}
-              isSubtitleRegular
-              isBoldWeight
-            />
-            <form className={styles.emailFormResponsive}>
-              <input
-                type="email"
-                id="email"
-                placeholder="Tu dirección de correo elctrónico"
-                required
-              />
-              <button type="submit">
-                <svg
-                  width="18"
-                  height="28"
-                  viewBox="0 0 18 28"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="Icon__StyledSVG-lm07h6-0 kNvokt"
-                >
-                  <path d="M1.825 28L18 14 1.825 0 0 1.715 14.196 14 0 26.285z"></path>
-                </svg>
-              </button>
-            </form>
-          </div>
-          <div className={styles.groupColumnResponsive}>
-            <FooterColumnResponsive
-              title={quienessomos.title}
-              items={quienessomos.items}
-            />
-            <FooterColumnResponsive
-              title={atencionAlCliente.title}
-              items={atencionAlCliente.items}
-            />
-            <FooterColumnResponsive
-              title={atracciones.title}
-              items={atracciones.items}
-            />
-            <FooterColumnResponsive
-              title={masImformacion.title}
-              items={masImformacion.items}
-            />
           </div>
         </div>
       </div>
       <div className={styles.mid}>
         <div className={styles.Container}>
           <div className={styles.email}>
-            <Tipografia
-              color={"--white"}
-              texto={"SUSCRÍBETE AL CORREO ELECTRÓNICO DE LEGO® SHOP"}
-              isSubtitleRegular
-              isBoldWeight
-            />
-            <form className={styles.emailForm}>
-              <input
-                type="email"
-                id="email"
-                placeholder="Tu dirección de correo elctrónico"
-                required
+            <a href=" ">
+              <Tipografia
+                color={"--white"}
+                texto={"SUSCRÍBETE AL CORREO ELECTRÓNICO"}
+                isBodyLarge
+                isMediumWeight
               />
-              <button type="submit">
-                <svg
-                  width="18"
-                  height="28"
-                  viewBox="0 0 18 28"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="Icon__StyledSVG-lm07h6-0 kNvokt"
-                >
-                  <path d="M1.825 28L18 14 1.825 0 0 1.715 14.196 14 0 26.285z"></path>
-                </svg>
-              </button>
-            </form>
+            </a>
+            <input
+              type="email"
+              id="email"
+              placeholder="Tu dirección de correo elctrónico"
+            ></input>
           </div>
           <div className={styles.rrss}>
-            <Tipografia
-              color={"--white"}
-              texto={"SÍGUENOS"}
-              isSubtitleRegular
-              isBoldWeight
-            />
+            <a href=" ">
+              <Tipografia
+                color={"--white"}
+                texto={"SÍGUENOS"}
+                isBodyLarge
+                isMediumWeight
+              />
+            </a>
             <div className={styles.iconos}>
               <Iconos
                 icono={<CgFacebook />}
@@ -507,7 +396,6 @@ const Footer = () => {
                 icono={<FaYoutube />}
                 href={"https://m.youtube.com/user/LEGO"}
               />
-
               {/* <Button isRedes={true} iconRight={<FaYoutube />} /> */}
             </div>
           </div>
@@ -517,19 +405,61 @@ const Footer = () => {
         <div className={styles.Container}>
           <div>
             <div className={styles.links}>
-              <FooterLinks links={links} />
+              <a href=" ">
+                <Tipografia
+                  color={"--white"}
+                  texto={"Cookies"}
+                  isBodySmall
+                  isRegularWeight
+                />
+              </a>
+              <a href=" ">
+                <Tipografia
+                  color={"--white"}
+                  texto={"Aviso legal"}
+                  isBodySmall
+                  isRegularWeight
+                />
+              </a>
+              <a href=" ">
+                <Tipografia
+                  color={"--white"}
+                  texto={"Clausulas de uso"}
+                  isBodySmall
+                  isRegularWeight
+                />
+              </a>
+              <a href=" ">
+                <Tipografia
+                  color={"--white"}
+                  texto={"Accesibilidad"}
+                  isBodySmall
+                  isRegularWeight
+                />
+              </a>
+              <a href=" ">
+                <Tipografia
+                  color={"--white"}
+                  texto={"Configuración de cookies"}
+                  isBodySmall
+                  isRegularWeight
+                />
+              </a>
             </div>
             <div />
             <div>
-              <p>
-                <Tipografia
-                  color={"--white"}
-                  texto={
-                    "LEGO System A/S, DK-7190 Billund, Dinamarca. Solo se permite la compra en línea a personas mayores de 18 años. LEGO, el logotipo LEGO, la minifigura, DUPLO, el logotipo de FRIENDS, el logotipo de MINIFIGURES, DREAMZzz, NINJAGO, VIDIYO y MINDSTORMS son marcas comerciales de The LEGO Group. ©2023 The LEGO Group. Todos los derechos reservados. El uso de este sitio supone la aceptación de las cláusulas de uso."
-                  }
-                  isBodySmall
-                />
-              </p>
+              <div>
+                <a href=" ">
+                  <Tipografia
+                    color={"--white"}
+                    texto={
+                      "LEGO System A/S, DK-7190 Billund, Dinamarca. Solo se permite la compra en línea a personas mayores de 18 años. LEGO, el logotipo LEGO, la minifigura, DUPLO, el logotipo de FRIENDS, el logotipo de MINIFIGURES, DREAMZzz, NINJAGO, VIDIYO y MINDSTORMS son marcas comerciales de The LEGO Group. ©2023 The LEGO Group. Todos los derechos reservados. El uso de este sitio supone la aceptación de las cláusulas de uso."
+                    }
+                    isBodySmall
+                    isRegularWeight
+                  />
+                </a>
+              </div>
             </div>
           </div>
         </div>
