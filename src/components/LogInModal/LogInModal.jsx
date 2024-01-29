f9572b925961261a0a3d3ab5ca2570a89cb08cc5
José Á. Calero M. <joseacalerom@gmail.com>
2024-01-29 12:45:53 +0100

    Updated LogIn check

--- src/components/LogInModal/LogInModal.jsx
+++ src/components/LogInModal/LogInModal.jsx
@@ -1,8 +1,41 @@
+import { useState } from "react";
 import Tipografia from "../Tipografia/Tipografia";
 import styles from "./LogInModal.module.scss";
 import { LegoLogo } from "../Footer/LegoLogo";
+import { useForm } from "../../hooks/useForm";
+import { usersListData } from "./UsersData.tsx";
 
 export const LogInModal = () => {
+  const checkLogin = (lista, currentUser) => {
+    const result = lista.filter((user) => {
+      return JSON.stringify(user) === JSON.stringify(currentUser);
+    });
+    if (result.length === 0 || result.length > 1) return false;
+    return true;
+  };
+
+  const initialForm = {
+    username: "",
+    password: "",
+  };
+
+  const [check, setCheck] = useState({
+    status: false,
+    error: false,
+    submit: false,
+  });
+
+  const { formState, username, password, onInputChange } = useForm(initialForm);
+
+  const onSubmitLogIn = (event) => {
+    event.preventDefault();
+    setCheck({
+      status: checkLogin(usersListData, formState),
+      error: !checkLogin(usersListData, formState),
+      submit: true,
+    });
+  };
+
   return (
     <>
       <div className={styles.logInModalContainer}>
@@ -18,7 +51,7 @@ export const LogInModal = () => {
                 isTitleBig
                 isBoldWeight
               />
-              <form onSubmit={() => {}}>
+              <form onSubmit={onSubmitLogIn}>
                 <div>
                   <label>
                     <Tipografia
@@ -28,7 +61,13 @@ export const LogInModal = () => {
                       isBoldWeight
                     />
                   </label>
-                  <input type="text" placeholder="Nombre de usuario"/>
+                  <input
+                    type="text"
+                    placeholder="Nombre de usuario"
+                    value={username}
+                    name="username"
+                    onChange={onInputChange}
+                  />
                   <a href="">
                     <Tipografia
                       color={"--blue"}
@@ -47,7 +86,13 @@ export const LogInModal = () => {
                       isBoldWeight
                     />
                   </label>
-                  <input type="password" placeholder="contraseña" />
+                  <input
+                    type="password"
+                    placeholder="contraseña"
+                    value={password}
+                    name="password"
+                    onChange={onInputChange}
+                  />
                   <a href="">
                     <Tipografia
                       color={"--blue"}
@@ -66,6 +111,13 @@ export const LogInModal = () => {
                   />
                 </button>
               </form>
+              {!check.submit ? (
+                ""
+              ) : check.error ? (
+                <h2>Datos inválidos ❌</h2>
+              ) : (
+                <h2>Datos válidos ✅</h2>
+              )}
             </div>
             <div className={styles.logInModalContainerBodyBottomNewUser}>
               <Tipografia
