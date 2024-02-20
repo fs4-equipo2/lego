d418cde93c1f8e3fcd91b2bc67c4b442f3ebc378
José Á. Calero M. <joseacalerom@gmail.com>
2024-02-20 11:11:24 +0100

    Update PaymentResult styles

--- src/pages/PaymentResultCancel.jsx
+++ src/pages/PaymentResultCancel.jsx
@@ -1,18 +1,31 @@
+import Tipografia from "../components/Tipografia/Tipografia";
 import { useStoreState, useStoreActions } from "../store";
+import styles from "./PaymentResult.module.scss";
 
 export const PaymentResultCancel = () => {
-
   const { user } = useStoreState((state) => state.user);
   const { setUser } = useStoreActions((actions) => actions.user);
 
-  window.onload = function() {
-    const valorAlmacenado = JSON.parse(sessionStorage.getItem('usuarioConectado'));
+  window.onload = function () {
+    const valorAlmacenado = JSON.parse(
+      sessionStorage.getItem("usuarioConectado")
+    );
     if (valorAlmacenado) {
-        setUser({...valorAlmacenado});
+      setUser({ ...valorAlmacenado });
     }
   };
 
   return (
-    <div>Resultado denegado de la compra</div>
-  )
-}
+    <>
+      <div className={styles.payResultCont}>
+        <Tipografia texto="Ha ocurrido un error." color={"--red"} isTitleLarge isBoldWeight />
+
+        <img
+          className={styles.payResultImg}
+          src="../../public/images/paymentResult/paymentError.png"
+          alt="Lego icon happy"
+        />
+      </div>
+    </>
+  );
+};
--- src/pages/PaymentResultOK.jsx
+++ src/pages/PaymentResultOK.jsx
@@ -1,18 +1,36 @@
+import Tipografia from "../components/Tipografia/Tipografia";
 import { useStoreState, useStoreActions } from "../store";
+import styles from "./PaymentResult.module.scss";
 
 export const PaymentResultOK = () => {
-
   const { user } = useStoreState((state) => state.user);
   const { setUser } = useStoreActions((actions) => actions.user);
 
-  window.onload = function() {
-    const valorAlmacenado = JSON.parse(sessionStorage.getItem('usuarioConectado'));
+  window.onload = function () {
+    const valorAlmacenado = JSON.parse(
+      sessionStorage.getItem("usuarioConectado")
+    );
     if (valorAlmacenado) {
-        setUser({...valorAlmacenado});
+      setUser({ ...valorAlmacenado });
     }
   };
 
   return (
-    <div>Resultado OK de la compra</div>
-  )
-}
+    <>
+      <div className={styles.payResultCont}>
+        <Tipografia
+          texto="¡Tu compra ha sido exitosa!"
+          color={"--green"}
+          isTitleLarge
+          isBoldWeight
+        />
+
+        <img
+          className={styles.payResultImg}
+          src="../../public/images/paymentResult/paymentOk.png"
+          alt="Lego icon happy"
+        />
+      </div>
+    </>
+  );
+};
