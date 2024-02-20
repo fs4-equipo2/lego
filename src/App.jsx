42e6615267700f9cbc7fd516d037d8281ab9f641
CantosCDenise <denisecantos2905@gmail.com>
2024-02-20 11:10:48 +0100

    blogs responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import { Blog } from "./components/Blog/Blog";
 import { BlogSelecciones } from "./components/BlogSelecciones/BlogSelecciones";
-import { BlogRecomendados } from "./components/BlogRecomendados/BlogRecomendados";
 import { Stickers } from "./components/Stickers/Stickers";
 import { WelcomeMessage } from "./components/WelcomeMessage/WelcomeMessage";
 import Main from "./components/Main/Main";
+import BlogRecomendados from "./components/BlogRecomendados/BlogRecomendados";
 function App() {
   return (
     <>
@@ -16,6 +16,8 @@ function App() {
       <Blog />
       <BlogSelecciones />
       <BlogRecomendados />
+      <br/>
+      <br/>
       <Blog />
       <WelcomeMessage />
       <br/>
