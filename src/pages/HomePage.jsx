dee2075f5c4799289236b15075eaf93056bee1f5
CantosCDenise <denisecantos2905@gmail.com>
2024-02-23 08:35:57 +0100

    added new sections

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,31 @@
 import Main from "../components/Main/Main.jsx";
 import { Stickers } from "../components/Stickers/Stickers";
 import { Blog } from "../components/Blog/Blog";
-import { BlogSelecciones } from "../components/BlogSelecciones/BlogSelecciones";
 import { BlogRecomendados } from "../components/BlogRecomendados/BlogRecomendados";
 import { WelcomeMessage } from "../components/WelcomeMessage/WelcomeMessage";
-
+import BlogPopulares from "../components/BlogPopulares/BlogPopulares.jsx";
+import BlogMasSets from "../components/BlogMasSets/BlogMasSets.jsx";
+import BlogMasSelecciones from "../components/BlogMasSelecciones/BlogMasSelecciones.jsx";
+import { BlogUnete } from "../components/BlogUnete/BlogUnete.jsx";
+// import {BlogSelecciones} from "../components/BlogSelecciones/BlogSelecciones.jsx"
 export const HomePage = () => {
   return (
     <>
       <Main />
       <Stickers />
-      <BlogSelecciones />
-      <Blog />
-      <BlogSelecciones />
+      {/* <BlogSelecciones /> */}
+      <BlogPopulares/>
+      <BlogUnete/>
       <BlogRecomendados />
+      <br/>
+      <br/>
+      <BlogMasSets/>
+      <BlogMasSelecciones/>
+      <br/>
+      <br/>
+      <Blog />
+      <WelcomeMessage />
+      <br/>
       <WelcomeMessage />
     </>
   )
