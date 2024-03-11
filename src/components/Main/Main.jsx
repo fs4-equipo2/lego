23ac4f8e5d0fb05b5c15790da76b0f86db4bca2f
Cami <camila.roserrano@gmail.com>
2024-03-11 22:09:34 +0100

    style: se arreglo el white-space de los botones

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,15 +4,15 @@ import Button from "../Button/Button";
 import { FaChevronRight } from "react-icons/fa6";
 import Logo from "../../assets/iconos/Logo";
 import Tipografia from "../Tipografia/Tipografia";
-import insidersLogo from "../../../public/images/imagenesMain/insidersLogo.png";    
-import backgroundMain from "../../../public/images/imagenesMain/backgroundMain.png";   
+import insidersLogo from "../../../public/images/imagenesMain/insidersLogo.png";
+import backgroundMain from "../../../public/images/imagenesMain/backgroundMain.png";
 import backgroundMainMobile from "../../../public/images/imagenesMain/backgroundMainMobile.png"
 
 
 const Main = () => {
   return (
     <div className={styles.containerGeneralMain}>
-      
+
       <div className={styles.containerBackgroundMain}>
         <img className={styles.backgroundMain} src={backgroundMain} />
         <img className={styles.backgroundMainMobile} src={backgroundMainMobile} />
@@ -29,15 +29,15 @@ const Main = () => {
 
         <div className={styles.containerTextoMain}>
           <div className={styles.tituloPrincipal}>
-          <Tipografia 
+          <Tipografia
               color={"--black"}
               isTitleXL={true}
               isboldWeight={true}
               texto={"Fin de semana LEGO® Insiders del 18/11 al 19/11"}
               />
-          </div>  
+          </div>
           <div className={styles.texto}>
-          <Tipografia 
+          <Tipografia
               color={"--black"}
               isSubtitleLarge={true}
               texto={"¿Que aún no eres miembro? ¡Únete hoy mismo y descubre las ofertas especiales!"}
